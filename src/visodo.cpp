fb07aefe7faa29f204a21cd60cff29f0475a3393
ashish-nr <ashishnair1993@yahoo.com>
2018-10-25 00:25:53 -0700

    making it EuRoC friendly part 1

--- src/visodo.cpp
+++ src/visodo.cpp
@@ -25,37 +25,77 @@ THE SOFTWARE.
 */
 
 #include "vo_features.h"
+#include <string>
+//#include <regex>
+#include <iomanip>
 
 using namespace cv;
 using namespace std;
 
-#define MAX_FRAME 1000
+#define MAX_FRAME 1709
 #define MIN_NUM_FEAT 2000
 
 // IMP: Change the file directories (4 places) according to where your dataset is saved before running!
+
+vector<string> split(string str, string token){
+    vector<string>result;
+    while(str.size()){
+        int index = str.find(token);
+        if(index!=string::npos){
+            result.push_back(str.substr(0,index));
+            str = str.substr(index+token.size());
+            if(str.size()==0)result.push_back(str);
+        }else{
+            result.push_back(str);
+            str = "";
+        }
+    }
+    return result;
+}
 
 double getAbsoluteScale(int frame_id, int sequence_id, double z_cal)	{
 
   string line;
   int i = 0;
-  ifstream myfile ( "/home/a/Programming/datasets/KITTI/odometry/poses/00.txt"/*"/home/avisingh/Datasets/KITTI_VO/00.txt"*/);
+  ifstream myfile ( "/home/a/Programming/datasets/V1_02_medium/mav0/state_groundtruth_estimate0/data.csv"/*"/home/avisingh/Datasets/KITTI_VO/00.txt"*/);
   double x =0, y=0, z = 0;
   double x_prev, y_prev, z_prev;
+  std::string a;
+  std::string::size_type sz;
+
   if (myfile.is_open())
   {
+    getline(myfile,line);
     while (( getline (myfile,line) ) && (i<=frame_id))
     {
       z_prev = z;
       x_prev = x;
       y_prev = y;
+
       std::istringstream in(line);
       //cout << line << '\n';
-      for (int j=0; j<12; j++)  {
-        in >> z ;
-        if (j==7) y=z;
-        if (j==3)  x=z;
+      size_t j=0;
+      while(j < 4)
+      {
+        //in >> z;
+        in >> a;
+        std::vector<string> result = split(a, ",");
+        //std::regex wsc_re("[\\s,]+"); // whitespace or comma
+        //std::vector<std::string> result{
+        //    std::sregex_token_iterator(a.begin(), a.end(), wsc_re, -1), {}
+        //};
+        //std::cout << std::endl;
+
+        for (size_t iter = 0; iter < 4; ++iter)
+        {
+            //std::cout << result[iter] << std::endl;
+            z = std::stod(result[iter], &sz);
+            if (j==2) y=z;
+            if (j==1)  x=z;
+            ++j;
+        }
       }
-      
+
       i++;
     }
     myfile.close();
@@ -75,15 +115,30 @@ int main( int argc, char** argv )	{
 
   Mat img_1, img_2;
   Mat R_f, t_f; //the final rotation and tranlation vectors containing the
+  std::vector<double> qtrnn(4); //0123 = xyzw
 
   ofstream myfile;
-  myfile.open ("results1_1.txt");
+  myfile.open ("pose_out.csv");
 
   double scale = 1.00;
   char filename1[200];
   char filename2[200];
-  sprintf(filename1, "/home/a/Programming/datasets/KITTI/odometry/sequences/00/image_2/%06d.png"/*"/home/avisingh/Datasets/KITTI_VO/00/image_2/%06d.png"*/, 0);
-  sprintf(filename2, "/home/a/Programming/datasets/KITTI/odometry/sequences/00/image_2/%06d.png"/*"/home/avisingh/Datasets/KITTI_VO/00/image_2/%06d.png"*/, 1);
+  sprintf(filename1, "/home/a/Programming/datasets/V1_02_medium/mav0/cam0/data/%19ld.png"/*"/home/avisingh/Datasets/KITTI_VO/00/image_2/%06d.png"*/, 1403715523912143104);
+  sprintf(filename2, "/home/a/Programming/datasets/V1_02_medium/mav0/cam0/data/%19ld.png"/*"/home/avisingh/Datasets/KITTI_VO/00/image_2/%06d.png"*/, 1403715523962142976);
+
+  std::ifstream input_ts_file;
+  input_ts_file.open("/home/a/Programming/datasets/V1_02_medium/mav0/cam0/timestamps.txt");
+  if(!input_ts_file)
+  {
+      std::cout<<"Error opening ts file"<< std::endl;
+      while(1);
+  }
+  std::string ts_line;
+  std::vector<long> eval_timestamps;
+  while(std::getline(input_ts_file, ts_line))
+  {
+      eval_timestamps.push_back(std::stol(ts_line));
+  }
 
   char text[100];
   int fontFace = FONT_HERSHEY_PLAIN;
@@ -111,8 +166,8 @@ int main( int argc, char** argv )	{
 
   //TODO: add a fucntion to load these values directly from KITTI's calib files
   // WARNING: different sequences in the KITTI VO dataset have different intrinsic/extrinsic parameters
-  double focal = /*458;*/718.8560;
-  cv::Point2d pp(/*367.215, 248.375*/607.1928, 185.2157);
+  double focal = 457.975;
+  cv::Point2d pp(367.215, 248.375);
   //recovering the pose and the essential matrix
   Mat E, R, t, mask;
   E = findEssentialMat(points2, points1, focal, pp, RANSAC, 0.999, 1.0, mask);
@@ -127,6 +182,7 @@ int main( int argc, char** argv )	{
 
   R_f = R.clone();
   t_f = t.clone();
+  //std::cout << R.rows << " " << R.cols << std::endl;
 
   clock_t begin = clock();
 
@@ -136,8 +192,10 @@ int main( int argc, char** argv )	{
   Mat traj = Mat::zeros(600, 600, CV_8UC3);
 
   for(int numFrame=2; numFrame < MAX_FRAME; numFrame++)	{
-  	sprintf(filename, "/home/a/Programming/datasets/KITTI/odometry/sequences/00/image_2/%06d.png"/*"/home/avisingh/Datasets/KITTI_VO/00/image_2/%06d.png"*/, numFrame);
-    //cout << numFrame << endl;
+  	sprintf(filename,
+            "/home/a/Programming/datasets/V1_02_medium/mav0/cam0/data/%19ld.png"/*"/home/avisingh/Datasets/KITTI_VO/00/image_2/%06d.png"*/,
+            eval_timestamps[numFrame]);
+    //cout << eval_timestamps[numFrame] << endl;
   	Mat currImage_c = imread(filename);
   	cvtColor(currImage_c, currImage, COLOR_BGR2GRAY);
   	vector<uchar> status;
@@ -161,28 +219,34 @@ int main( int argc, char** argv )	{
 
     //cout << "Scale is " << scale << endl;
 
-    if ((scale>0.1)&&(t.at<double>(2) > t.at<double>(0)) && (t.at<double>(2) > t.at<double>(1))) {
+    //if ((scale>0.1)&&(t.at<double>(2) > t.at<double>(0)) && (t.at<double>(2) > t.at<double>(1))) { //bogus
 
-      t_f = t_f + scale*(R_f*t);
+      t_f = t_f + 10*scale*(R_f*t);
       R_f = R*R_f;
 
-    }
-  	
-    else {
+    //}
+
+    //else {
      //cout << "scale below 0.1, or incorrect translation" << endl;
-    }
-    
-   // lines for printing results
-   // myfile << t_f.at<double>(0) << " " << t_f.at<double>(1) << " " << t_f.at<double>(2) << endl;
+    //}
 
-  // a redetection is triggered in case the number of feautres being trakced go below a particular threshold
- 	  if (prevFeatures.size() < MIN_NUM_FEAT)	{
-      //cout << "Number of tracked features reduced to " << prevFeatures.size() << endl;
-      //cout << "trigerring redection" << endl;
- 		  featureDetection(prevImage, prevFeatures);
-      featureTracking(prevImage,currImage,prevFeatures,currFeatures, status);
+    qtrnn[3] = std::sqrt(1.0 + R_f.at<double>(0,0) + R_f.at<double>(1,1) + R_f.at<double>(2,2)) / 2.0;
+	double w4 = (4.0 * qtrnn[3]);
+	qtrnn[0] = (R_f.at<double>(2,1) - R_f.at<double>(1,2)) / w4 ;
+	qtrnn[1] = (R_f.at<double>(0,2) - R_f.at<double>(2,0)) / w4 ;
+	qtrnn[2] = (R_f.at<double>(1,0) - R_f.at<double>(0,1)) / w4 ;
 
- 	  }
+    // lines for printing results
+    myfile << std::setprecision(19) << eval_timestamps[numFrame] << "," << t_f.at<double>(0) << "," << t_f.at<double>(1) << "," << t_f.at<double>(2) << "," << qtrnn[3] << "," << qtrnn[0] << "," << qtrnn[1] << "," << qtrnn[2] << endl;
+
+    // a redetection is triggered in case the number of feautres being trakced go below a particular threshold
+ 	if (prevFeatures.size() < MIN_NUM_FEAT)	{
+    //cout << "Number of tracked features reduced to " << prevFeatures.size() << endl;
+    //cout << "trigerring redection" << endl;
+ 	 featureDetection(prevImage, prevFeatures);
+    featureTracking(prevImage,currImage,prevFeatures,currFeatures, status);
+
+ 	}
 
     prevImage = currImage.clone();
     prevFeatures = currFeatures;
